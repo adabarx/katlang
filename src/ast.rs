024bf1801906eebf80218fc7fb35d7b6b8d944ca
Katlyn Thomas <catlin.a.thomas@gmail.com>
2023-06-06 22:32:34 -0500

    moving stuff around

--- src/ast.rs
+++ src/ast.rs
@@ -24,6 +24,7 @@ pub enum Expression {
     },
     FunctionCall {
         callee: String,
+        signature: (TypeExpression, TypeExpression),
         arguments: Vec<Expression>
     },
     Literal(Literal),
@@ -227,24 +228,30 @@ impl Parser {
     }
 
     fn parse_statement(&mut self) -> Result<ASTNode> {
-        Ok(match self.input.get(self.position).ok_or(anyhow!("EOF"))? {
-            Token::Import       => self.p_import_statement()?,
-            Token::Let          => ASTNode::Declaration(self.p_variable_declaration()?),
-            Token::Def          => ASTNode::Declaration(self.p_function_declaration()?),
-            Token::Struct       => ASTNode::Declaration(self.p_struct_declaration()?),
-            Token::Enum         => ASTNode::Declaration(self.p_enum_declaration()?),
-            Token::Object       => ASTNode::Declaration(self.p_object_declaration()?),
-            Token::If           => self.p_if_statement()?,
-            Token::Elif         => self.p_elif_statement()?,
-            Token::Match        => self.p_match_statement()?,
+        let rv = match &self.token {
+            Token::Import => self.parse_import_statement()?,
+            Token::Let    => ASTNode::Declaration(self.parse_variable_declaration()?),
+            Token::Struct => ASTNode::Declaration(self.parse_struct_declaration()?),
+            Token::Enum   => ASTNode::Declaration(self.parse_enum_declaration()?),
+            Token::Object => ASTNode::Declaration(self.parse_object_declaration()?),
+            Token::If     => self.parse_if_statement()?,
+            Token::Elif   => self.parse_elif_statement()?,
+            Token::Match  => self.parse_match_statement()?,
+            Token::Def    => 
+                if let Token::Ident(ident) = self.next_token()?
+                    { ASTNode::Declaration(self.parse_function_declaration()?) }
+                else
+                    { ASTNode::Expression(self.parse_anonymouse_function()?) },
             Token::Ident(ident) => ASTNode::Expression(
                 self.parse_expression(Some(Expression::Variable(ident.clone())))?
             ),
             _ => bail!("syntax error: parse_statement")
-        })
+        };
+        self.next_token()?;
+        Ok(rv)
     }
 
-    fn p_variable_declaration(&mut self) -> Result<Declaration> {
+    fn parse_variable_declaration(&mut self) -> Result<Declaration> {
         // let x = val
         // let mut x = val
         Ok(match self.next_token()? {
@@ -257,7 +264,7 @@ impl Parser {
                 } else {
                     todo!("implement else")
                 }
-                _ => bail!("syntax error: p_variable_declaration")
+                _ => bail!("syntax error: parse_variable_declaration")
             },
             Token::Ident(name) => if self.next_token()? == Token::Equal {
                 Declaration::Variable {
@@ -267,7 +274,7 @@ impl Parser {
             } else {
                 todo!("implement else")
             },
-            _ => bail!("syntax error: p_variable_declaration")
+            _ => bail!("syntax error: parse_variable_declaration")
         })
     }
 
@@ -281,7 +288,7 @@ impl Parser {
             Some(Expression::Unary { operator, expression }) => todo!("impl unary"),
             Some(Expression::Binary { operator, lhs, rhs }) => todo!("impl binary"),
             Some(Expression::Literal(lit)) => todo!("impl literal"),
-            Some(Expression::FunctionCall { callee, arguments }) => todo!("impl function call"),
+            Some(Expression::FunctionCall { callee, signature, arguments }) => todo!("impl function call"),
             Some(Expression::MemberAccess { object, property }) => todo!("impl member access"),
             None => {
                 match self.next_token()? {
@@ -311,48 +318,8 @@ impl Parser {
                             operator: Operator::LogicalInverse,
                             expression: Box::new(self.parse_expression(None)?)
                         },
-                    Token::Ident(ident) =>
-                        match self.next_token()? {
-                            Token::Ident(param) =>
-                                Expression::FunctionCall {
-                                    callee: ident,
-                                    arguments: vec![Expression::Variable(param)]
-                                },
-                            Token::SemiColon => 
-                                if self.next_token()? == Token::SemiColon {
-                                    Expression::Unary {
-                                        operator: Operator::ReturnNull,
-                                        expression: Box::new(Expression::Variable(ident))
-                                    }
-                                } else {
-                                    bail!("syntax error: double semicolon")
-                                },
-                            Token::Question =>
-                                Expression::Unary {
-                                    operator: Operator::ReturnNull,
-                                    expression: Box::new(Expression::Variable(ident))
-                                },
-                            Token::Plus => todo!("binary add"),
-                            Token::Dash => todo!("binary subtract"),
-                            Token::Asterisk => todo!("binary multiply"),
-                            Token::FSlash => todo!("binary divide"),
-                            Token::Percent => todo!("binary modulus"),
-                            Token::Bang => todo!("binary not equal compare"),
-                            Token::Equal => todo!("binary equal compare"),
-                            Token::LAngle => todo!("binary less/less-equal than"),
-                            Token::RAngle => todo!("binary greater/greater-equal than"),
-                            Token::And => todo!("binary and"),
-                            Token::Ampersand => todo!("binary double amp and"),
-                            Token::Or => todo!("binary or"),
-                            Token::Pipe => todo!("binary double pipe or"),
-                            Token::Xor => todo!("binary xor"),
-                            Token::Caret => todo!("binary double caret xor"),
-                            Token::Dot => todo!("binary member access"),
-
-                            Token::LParen => self.parse_tuple()?,
-                            _ => bail!("syntax error: p_variable_declaration")
-                        }
-                    _ => bail!("syntax error: p_variable_declaration")
+                    Token::Ident(ident) => self.match_token_ident(ident)?,
+                    _ => bail!("syntax error: parse_variable_declaration")
                 }
             }
         });
@@ -361,16 +328,65 @@ impl Parser {
         else { self.parse_expression(Some(rv?)) }
     }
 
-    fn parse_tuple(&mut self) -> Result<Expression> { todo!("parse tuple") }
-    fn p_identifier_statement(&mut self, _ident: &String) -> Result<Expression> { bail!("todo") }
-    fn p_function_declaration(&mut self) -> Result<Declaration> { bail!("todo") }
-    fn p_struct_declaration(&mut self) -> Result<Declaration> { bail!("todo") }
-    fn p_enum_declaration(&mut self) -> Result<Declaration> { bail!("todo") }
-    fn p_object_declaration(&mut self) -> Result<Declaration> { bail!("todo") }
-    fn p_if_statement(&mut self) -> Result<ASTNode> { bail!("todo") }
-    fn p_elif_statement(&mut self) -> Result<ASTNode> { bail!("todo") }
-    fn p_match_statement(&mut self) -> Result<ASTNode> { bail!("todo") }
-    fn p_import_statement(&mut self) -> Result<ASTNode> { bail!("todo") }
-    fn parse_parameters(&mut self) -> Result<Vec<Expression>> { bail!("todo") }
+    fn match_token_ident(&mut self, ident: String) -> Result<Expression> {
+        Ok(match self.next_token()? {
+            Token::Ident(param) =>
+                Expression::FunctionCall {
+                    callee: ident,
+                    signature: (
+                        TypeExpression::Unary(Box::new(Type { name: "i32".to_string(), enclosed: None })),
+                        TypeExpression::Unary(Box::new(Type { name: "i32".to_string(), enclosed: None }))
+                    ),
+                    arguments: vec![Expression::Variable(param)]
+                },
+            Token::SemiColon => 
+                if self.next_token()? == Token::SemiColon {
+                    Expression::Unary {
+                        operator: Operator::ReturnNull,
+                        expression: Box::new(Expression::Variable(ident))
+                    }
+                } else {
+                    bail!("syntax error: double semicolon")
+                },
+            Token::Question =>
+                Expression::Unary {
+                    operator: Operator::ReturnNull,
+                    expression: Box::new(Expression::Variable(ident))
+                },
+            Token::Plus => todo!("binary add"),
+            Token::Dash => todo!("binary subtract"),
+            Token::Asterisk => todo!("binary multiply"),
+            Token::FSlash => todo!("binary divide"),
+            Token::Percent => todo!("binary modulus"),
+            Token::Bang => todo!("binary not equal compare"),
+            Token::Equal => todo!("binary equal compare"),
+            Token::LAngle => todo!("binary less/less-equal than"),
+            Token::RAngle => todo!("binary greater/greater-equal than"),
+            Token::And => todo!("binary and"),
+            Token::Ampersand => todo!("binary double amp and"),
+            Token::Or => todo!("binary or"),
+            Token::Pipe => todo!("binary double pipe or"),
+            Token::Xor => todo!("binary xor"),
+            Token::Caret => todo!("binary double caret xor"),
+            Token::Dot => todo!("binary member access"),
+
+            Token::LParen => self.parse_tuple()?,
+            _ => bail!("syntax error: parse_variable_declaration")
+        })
+    }
+
+    fn parse_tuple(&mut self) -> Result<Expression> { todo!("parse_tuple") }
+    fn parse_identifier_statement(&mut self, _ident: &String) -> Result<Expression> { todo!("parse_identifier_statement") }
+    fn parse_function_declaration(&mut self) -> Result<Declaration> { todo!("parse_function_declaration") }
+    fn parse_struct_declaration(&mut self) -> Result<Declaration> { todo!("parse_struct_declaration") }
+    fn parse_enum_declaration(&mut self) -> Result<Declaration> { todo!("parse_enum_declaration") }
+    fn parse_object_declaration(&mut self) -> Result<Declaration> { todo!("parse_object_declaration") }
+    fn parse_if_statement(&mut self) -> Result<ASTNode> { todo!("parse_if_statement") }
+    fn parse_elif_statement(&mut self) -> Result<ASTNode> { todo!("parse_elif_statement") }
+    fn parse_match_statement(&mut self) -> Result<ASTNode> { todo!("parse_match_statement") }
+    fn parse_import_statement(&mut self) -> Result<ASTNode> { todo!("parse_import_statement") }
+    fn parse_parameters(&mut self) -> Result<Vec<Expression>> { todo!("parse_parameters") }
+    fn parse_arguments(&mut self) -> Result<Vec<Expression>> { todo!("parse_arguments") }
+    fn parse_anonymouse_function(&mut self) -> Result<Expression> { todo!("parse_anonymous_function") }
 }
 
