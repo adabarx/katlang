a910a8412ee8c8be427267ab96bce2c066096406
katlyn-thomas <katlyn.c.thomas@gmail.com>
2023-09-29 15:33:00 -0500

    added filepath arg

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,20 @@
+use clap::{command, Arg};
+
 mod lexer;
 mod ast;
 
 use lexer::Lexer;
 use ast::Parser;
 
+
 // 1: tokenize into basic symbols, keywords, and literals
 // 2: parse into abstract syntax tree
 
 fn main() {
-    if let Ok(input) = std::fs::read_to_string("test_file.kat") {
+    let result = command!().arg(Arg::new("filepath").required(true))
+        .get_matches();
+    let file_path = result.get_one::<String>("filepath").unwrap();
+    if let Ok(input) = std::fs::read_to_string(file_path) {
         // 1: tokenize into basic symbols, keywords, and literals
         let mut lexer = Lexer::new(input);
         let mut tokens = vec![];
@@ -20,7 +26,7 @@ fn main() {
         }
         dbg!(&tokens);
 
-        let mut parser = Parser::new(tokens);
+        let parser = Parser::new(tokens);
         let astree = parser.lfg();
 
         dbg!(&astree);
